f979401e0318a6effd421ad9ee1b45b08c61b1dc
Artyom <artyomsivash@gmail.com>
2022-03-07 18:41:23 +0200

    2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const form = document.querySelector('.form');
 const input = document.querySelector('.form__input');
+const navBtn = document.querySelector('.nav__button');
+const mobileNav = document.querySelector('.nav__mobile-nav');
+
 
 input.addEventListener('focus', function () {
    form.classList.add('form--active');
@@ -8,4 +11,10 @@ input.addEventListener('focus', function () {
 
 input.addEventListener('blur', function () {
    form.classList.remove('form--active');
+})
+
+
+navBtn.addEventListener('click', function () {
+   mobileNav.classList.toggle('nav__mobile-nav--active');
+   navBtn.classList.toggle('nav__button--close');
 })
