4f0ef61baa6ef1422cb4ecbbe86a1b93f3bfe1a0
Artyom <artyomsivash@gmail.com>
2022-03-07 19:17:27 +0200

    5

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const form = document.querySelector('.form');
 const input = document.querySelector('.form__input');
 const navBtn = document.querySelector('.nav__button');
 const mobileNav = document.querySelector('.nav__mobile-nav');
+const body = document.body;
 
 
 input.addEventListener('focus', function () {
@@ -14,7 +15,23 @@ input.addEventListener('blur', function () {
 })
 
 
-navBtn.addEventListener('click', function () {
+navBtn.addEventListener('click', function (event) {
+   event.stopPropagation();
    mobileNav.classList.toggle('nav__mobile-nav--active');
    navBtn.classList.toggle('nav__button--close');
+   body.classList.toggle('no-scroll');
+})
+
+window.addEventListener('click', function () {
+   if (body.classList.contains('no-scroll')) {
+      body.classList.toggle('no-scroll');
+      mobileNav.classList.toggle('nav__mobile-nav--active');
+      navBtn.classList.toggle('nav__button--close');
+   }
+
+})
+
+mobileNav.addEventListener('click', function (event) {
+   event.stopPropagation();
+
 })
